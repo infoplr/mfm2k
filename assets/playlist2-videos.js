3b0a21d9f5a4d1948d0006db04ac4b84a9a0c939
Céo <noah.kd@gmail.com>
2022-09-22 13:20:54 -0300

    FUIOG

---
+++ assets/playlist2-videos.js
@@ -0,0 +1,31 @@
+export default [
+    {
+      src: "../04. Levantar Caixa Rápido/1. Como a estratégia funciona.mp4",
+      title: "Editando fotos",
+    },
+    {
+      src: "./img/02.mp4",
+      title: "Live Stream",
+    },
+    {
+      src: "./img/03.mp4",
+      title: "Digitando",
+    },
+    {
+      src: "./img/04.mp4",
+      title: "Home office",
+    },
+    {
+      src: "./img/05.mp4",
+      title: "Likes",
+    },
+    {
+      src: "./img/06.mp4",
+      title: "Gota de água",
+    },
+    {
+      src: "./img/07.mp4",
+      title: "Código",
+    },
+  ];
+  
