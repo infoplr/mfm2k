9d025297d2417f2b39a21f20a647d254b1a5caec
Céo <noah.kd@gmail.com>
2022-09-23 17:58:49 -0300

    IHYGIOPO

---
+++ notations.js
@@ -0,0 +1,75 @@
+
+
+/* modulo 5
+=============================== */
+// aquecimento de bm e contas de anúncios
+
+var dia01 = function criarABmPelo( whatsapp ) {
+    passo1 = no( celular ) {
+        "- instale o business whatsapp";
+        "- consiga um numero e use no whatsapp";
+    },
+    passo2 = na( fanPage ) {
+        "- > configs > whatsapp";
+        "- use o numero do whatsapp";
+    }
+    return whatsappVinculado && BMCriada;
+
+    passo3 = acessar( "business.facebook.com/settings" ) {
+        "criar um novo pixel";
+    }
+},
+dia02 = fazerShieldNaBm( 
+    "1 - preencher as informações da empresa",
+    "2 - autenticação de dois fatores",
+    "3 - adicionar forma de pagamento",
+    "4 - criar a primeira conta de anúncio - e adicionar a forma de pagamento do gerenciador de negócios",
+    criarAplicativo(
+        "crie uma conta de dev em developers.facebook.com",
+        "em About you: escolha: gerente de produto",
+        "vai em criar aplicativo:",
+        "tipo: empresa",
+        "nome de exibição: o mesmo nome da fan page",
+        "vai pedir a senha do seu perfil",
+        "vai em configs:",
+        "basico:",
+        "em namespace: nome inteiro da fan page",
+        "em informações de contato do encarregado: preencha com as mesmas informações do campo de empresa da bm",
+        "depois de tudo, copia o id do aplicativo, e usar no gerenciador do negócio",
+        "e adicione ativos: e de acesso para a conta de anúncio"
+    ),
+    criarContaDoInstagramEVincularNaBm(
+        "nome completo: nome da fan page",
+        "nome de usuário: qualquer relacionado a fan page",
+        "coloca a mesma foto da fan page na foto de perfil",
+        "adiciona no gerenciador e mude para uma conta comercial",
+        "clica em adicionar ativos, e em concluir"
+    ),
+    criarUmGrupoDeAtivosDeNegocios(
+        "marcas separadas ou linhas de negócios",
+        "mesmo nome da fan page",
+        "abilite todas as coisas",
+    ),
+    adicionarUmNovoUsuarioDeSistema(
+        "qualquer nome",
+        "função de administrador"
+    ),
+    adicionarDuasNovasPessoas(
+        ""
+    )
+ ),
+ subirCampanhaDeEngajamentoNa( Conta1 ) {
+    "7 reais por dia por 2 à 3 dias",
+    "use uma das publicações da fan page"
+ }
+
+ dia03 = [
+    cobrançaNa( Conta01 ),
+    criarAsOutrasContasDeAnuncio,
+    subirAPrimeiraCampanhaDe( conversão ) {
+        "com um valor mais baixo que o da campanha de engajamento ( 6 reais )",
+        "vai aumentando o valor com o passar dos dias"
+    }
+ ]
+// docs.google.com/document/d/1ey0NgqIjCkBc8Z38U3mJ1vRUseH4NCCOWWsIgx9d/0g/edit
+
